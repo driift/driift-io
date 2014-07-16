25576a1e77233e4ed1b43c01ed56b322dfc28268
Chris Waring <chris@wwaves.co>
2014-07-16 17:26:34 +0100

    add rangeslider

--- client/pricing/pricing.js
+++ client/pricing/pricing.js
@@ -6,3 +6,28 @@ PricingController = RouteController.extend({
     }
   }
 })
+
+Template.pricing.rendered = function() {
+ $('input[data-rangeslider]').rangeslider({
+
+    // Feature detection the default is `true`.
+    // Set this to `false` if you want to use
+    // the polyfill also in Browsers which support
+    // the native <input type="range"> element.
+    polyfill: false,
+
+    // Default CSS classes
+    rangeClass: 'rangeslider',
+    fillClass: 'rangeslider__fill',
+    handleClass: 'rangeslider__handle',
+
+    // Callback function
+    onInit: function() {},
+
+    // Callback function
+    onSlide: function(position, value) {},
+
+    // Callback function
+    onSlideEnd: function(position, value) {}
+  });
+}
