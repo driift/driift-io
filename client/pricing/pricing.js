6df58becf9428a0105c10860235b98f1da6dbc4d
Chris Waring <chris@wwaves.co>
2014-07-18 12:24:25 +0100

    copy and draft calcs

--- client/pricing/pricing.js
+++ client/pricing/pricing.js
@@ -25,7 +25,17 @@ Template.pricing.rendered = function() {
     onInit: function() {},
 
     // Callback function
-    onSlide: function(position, value) {},
+    onSlide: function(position, value) {
+      var discount = [5,10,15,20];
+      $('.pricing-attedees').text(value)
+      if(value > 500)
+        $('.pricing-discount').text(discount[0] + '%')
+      if(value > 1000)
+        $('.pricing-discount').text(discount[1] + '%')
+      if(value > 5000)
+        $('.pricing-discount').text(discount[2] + '%')
+      $('.pricing-total').text('£' + value*12)
+    },
 
     // Callback function
     onSlideEnd: function(position, value) {}
